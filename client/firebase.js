a0f3b6858cf520ed3f62e6e56d8aa8f534cb03a6
Abdelghani Bensalih <abdelghaniben40@gmail.com>
2024-08-13 19:44:58 +0200

    initialled firebase, added API foulder: generate for flash cards generation and strip for getting strip session

---
+++ client/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC1dXOZicweh40D7BbEJDXQvRj_GcbaV3o",
+  authDomain: "ai-flashcards1.firebaseapp.com",
+  projectId: "ai-flashcards1",
+  storageBucket: "ai-flashcards1.appspot.com",
+  messagingSenderId: "188810493733",
+  appId: "1:188810493733:web:6b2b77fd1974dcf6e6e4cc",
+  measurementId: "G-B8DB42X9T6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
