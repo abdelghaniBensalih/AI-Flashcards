d6bfa6ebad49258b7b53fc939cbea3a718614a5f
Github11200 <pateljinay@outlook.com>
2024-08-13 19:58:29 -0700

    small changes

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -10,8 +10,6 @@ import {
   SignUpButton,
   useUser,
 } from "@clerk/nextjs";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function Home() {
