729a4c70a2201e9f391e23c5f04c36f73dcd391b
Github11200 <pateljinay@outlook.com>
2024-08-13 23:21:01 -0700

    added the ability to generate cards usign emini

--- client/app/api/generate/route.ts
+++ client/app/api/generate/route.ts
@@ -3,12 +3,34 @@ import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-1.5-flash",
   maxOutputTokens: 2048,
+  apiKey: process.env.GEMINI_API_KEY,
 });
 
-// Batch and stream are also supported
-const res = await model.invoke([
-  [
-    "human",
-    "What would be a good company name for a company that makes colorful socks?",
-  ],
-]);
+const prompt = `
+  You are an intelligent AI that can will generate flashcards based on a description.
+
+  The description will be given to you as a string.
+
+  You must return JSON data with the following schema, JUST INCLUDE THE JSON, DO NOT ADD JSON AT THE START OR ADD BACKTICKS:
+  
+  {
+    cards: [{
+      "front": "Front of the flashcard",
+      "back": "Back of the flashcard"
+    }],
+    name: "Provide a consise name for the deck that is less than 3 words",
+    description: "Provide a description for the deck"
+  }
+`;
+
+export async function POST(request: Request) {
+  const deckDescription: string = (await request.json()).description;
+
+  // Batch and stream are also supported
+  const res = await model.invoke([
+    ["system", prompt],
+    ["human", deckDescription],
+  ]);
+
+  return new Response(res.content as BodyInit);
+}
--- client/app/dashboard/newDeck/page.tsx
+++ client/app/dashboard/newDeck/page.tsx
@@ -4,10 +4,31 @@ import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { ChevronLeft } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { createDeck } from "@/lib/firebase/crud";
+import { Deck } from "@/lib/interfaces/interfaces";
+import { Card } from "@/components/ui/card";
 
 export default function Page() {
+  const [description, setDescription] = useState("");
+  const [deck, setDeck] = useState<Deck>();
+
+  const handleSubmit = async () => {
+    fetch("/api/generate", {
+      method: "POST",
+      body: JSON.stringify({ description }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setDeck(data);
+      });
+  };
+
   return (
-    <div>
+    <div className="grid gap-10">
       {" "}
       <div className="flex flex-row items-center justify-between">
         <Link href="/dashboard">
@@ -21,6 +42,53 @@ export default function Page() {
         <div>
           <UserButton />
         </div>
+      </div>
+      <div>
+        <div className="grid gap-2 w-96">
+          <Label htmlFor="description">Deck Description</Label>
+          <Textarea
+            placeholder="Create a deck about planets..."
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            onKeyUp={(e) => {
+              if (e.key === "Enter") handleSubmit();
+            }}
+          />
+          <Button
+            className="max-w-min justify-self-end"
+            onClick={() => handleSubmit()}
+          >
+            Generate!
+          </Button>
+        </div>
+      </div>
+      <div className="w-full flex flex-wrap gap-4 justify-center">
+        {deck &&
+          deck.cards.map((card, index) => {
+            return (
+              <div className="flip-card">
+                <div className="flip-card-inner">
+                  <Card
+                    key={index}
+                    className="size-60 flex items-center justify-center hover:hidden flip-card-front"
+                  >
+                    <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight first:mt-0 text-center">
+                      {card.front}
+                    </h2>
+                  </Card>
+                  <Card
+                    key={index}
+                    className="size-60 flex items-center justify-center absolute flip-card-back p-4"
+                  >
+                    <h2 className="scroll-m-20 text-lg font-semibold tracking-tight first:mt-0 text-center">
+                      {card.back}
+                    </h2>
+                  </Card>
+                </div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
