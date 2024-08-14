1d9284e0cec40735a8d7ac4c6a24df85a4ca8810
nebilawako <nebilawako@gmail.com>
2024-08-14 01:32:19 -0600

    slider to make navigation between cards easier when we have large numbers

--- client/app/dashboard/[deck]/page.tsx
+++ client/app/dashboard/[deck]/page.tsx
@@ -44,6 +44,11 @@ export default function Page() {
       setCurrentCardIndex(currentCardIndex - 1);
     }
   };
+  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const index = parseInt(event.target.value, 10);
+    setIsFlipped(false);
+    setCurrentCardIndex(index);
+  };
 
 
   useEffect(() => {
@@ -53,37 +58,75 @@ export default function Page() {
   }, [userId]);
 
   // return (
-  //   <div>
-  //     <div className="flex flex-row items-center justify-between">
+  //   <div className="flex flex-col items-center h-screen">
+  //     <div className="flex flex-row items-center justify-between w-full p-4">
   //       <Link href="/dashboard">
-  //         <Button variant="outline" className="">
+  //         <Button variant="outline">
   //           <ChevronLeft />
   //         </Button>
-  //       </Link>{" "}
-  //       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
+  //       </Link>
+  //       <h1 className="text-4xl font-extrabold tracking-tight text-center">
   //         {deckName}
   //       </h1>
-  //       <div>
+  //       <div className="flex items-center space-x-4">
+  //         <ModeToggle /> 
   //         <UserButton />
   //       </div>
+     
   //     </div>
-  //     <div>
-  //       {deck &&
-  //         deck.cards.map((card, index) => {
-  //           return (
-  //             <div key={index}>
-  //               <h1>{card.front}</h1>
-  //               <h2>{card.back}</h2>
+
+  //     {deck && deck.cards.length > 0 ? (
+  //       <div className="flex flex-col items-center w-full">
+  //         <Card
+  //           className={`w-full max-w-4xl h-[32rem] text-center cursor-pointer transition-transform duration-700 mt-10 ${
+  //             isFlipped ? "transform rotate-y-180" : ""
+  //           }`}
+  //           onClick={handleFlip}
+  //         >
+        
+  //           <CardContent className="flex flex-col justify-center items-center h-full">
+  //             <div>
+  //               {isFlipped ? (
+  //                 <div className="text-4xl">
+  //                   <p>{deck.cards[currentCardIndex].back}</p>
+  //                 </div>
+  //               ) : (
+  //                 <div className="text-4xl">
+  //                   <p>{deck.cards[currentCardIndex].front}</p>
+  //                 </div>
+  //               )}
   //             </div>
-  //           );
-  //         })}
-  //     </div>
+  //             <p className="mt-4">Click card to flip</p>
+  //           </CardContent>
+  //           <CardFooter className="text-center">
+  //             <p className="mt-4">Click card to flip</p>
+  //           </CardFooter>
+         
+  //         </Card>
+
+  //         <div className="flex justify-between items-center mt-4 w-80">
+  //           <Button onClick={handlePreviousCard} disabled={currentCardIndex === 0}>
+  //             ← Previous
+  //           </Button>
+  //           <p>
+  //             {currentCardIndex + 1}/{deck.cards.length}
+  //           </p>
+  //           <Button
+  //             onClick={handleNextCard}
+  //             disabled={currentCardIndex === deck.cards.length - 1}
+  //           >
+  //             Next →
+  //           </Button>
+  //         </div>
+  //       </div>
+  //     ) : (
+  //       <p>No cards available for this deck.</p>
+  //     )}
   //   </div>
   // );
-
   return (
     <div className="flex flex-col items-center h-screen">
-      <div className="flex flex-row items-center justify-between w-full p-4">
+      <div className="flex flex-row items-center justify-between w-full p-4 min-h-[4rem]">
         <Link href="/dashboard">
           <Button variant="outline">
             <ChevronLeft />
@@ -96,33 +139,28 @@ export default function Page() {
           <ModeToggle /> {/* Add the ModeToggle component here */}
           <UserButton />
         </div>
-        {/* <UserButton /> */}
       </div>
 
       {deck && deck.cards.length > 0 ? (
         <div className="flex flex-col items-center w-full">
           <Card
-            className={`w-full max-w-4xl h-[32rem] text-center cursor-pointer transition-transform duration-700 mt-10 ${
-              isFlipped ? "transform rotate-y-180" : ""
-            }`}
+            className="w-full max-w-4xl h-[32rem] text-center cursor-pointer transition-transform duration-700 mt-10 flex flex-col"
             onClick={handleFlip}
           >
-        
-            <CardContent className="flex flex-col justify-center items-center h-full">
-              <div>
+            <CardContent className="flex-grow flex items-center justify-center">
+              <div className="text-4xl">
                 {isFlipped ? (
-                  <div className="text-4xl">
-                    <p>{deck.cards[currentCardIndex].back}</p>
-                  </div>
+                  <p>{deck.cards[currentCardIndex].back}</p>
                 ) : (
-                  <div className="text-4xl">
-                    <p>{deck.cards[currentCardIndex].front}</p>
-                  </div>
+                  <p>{deck.cards[currentCardIndex].front}</p>
                 )}
               </div>
-              <p className="mt-4">Click card to flip</p>
             </CardContent>
-         
+            <div className="border-t mt-auto">
+              <div className="text-center p-2">
+                <p>Click card to flip</p>
+              </div>
+            </div>
           </Card>
 
           <div className="flex justify-between items-center mt-4 w-80">
@@ -139,7 +177,16 @@ export default function Page() {
               Next →
             </Button>
           </div>
+          <input
+              type="range"
+              min="0"
+              max={deck.cards.length - 1}
+              value={currentCardIndex}
+              onChange={handleSliderChange}
+              className="w-full mt-4"
+            />
         </div>
+        
       ) : (
         <p>No cards available for this deck.</p>
       )}
