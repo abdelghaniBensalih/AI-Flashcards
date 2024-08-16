5db583b06fd968a5db3a8f17831f87018f31b84e
Github11200 <pateljinay@outlook.com>
2024-08-15 20:43:08 -0700

    merged

--- client/app/api/generateDeckFromImage/route.ts
+++ client/app/api/generateDeckFromImage/route.ts
@@ -1,14 +1,16 @@
 import { Client } from "@gradio/client";
 
 export async function POST(request: Request) {
-  const response_0 = await fetch(
-    "https://pm1.aminoapps.com/7551/f4ce06bbee3c9f6d23baf4eaec0286a190db140ar1-2048-2048v2_uhq.jpg"
-  );
-  const exampleImage = await response_0.blob();
+  const formData = await request.formData();
+  const image = formData.get("image") as File;
+
+  const imageReader = image.stream().getReader();
+  const { value } = await imageReader.read();
+  const blob = new Blob([value!]);
 
   const client = await Client.connect("jp12223/jinhybr-OCR-Donut-CORD");
   const result = await client.predict("/predict", {
-    param_0: exampleImage,
+    param_0: blob,
   });
 
   return new Response(JSON.stringify(result.data));
